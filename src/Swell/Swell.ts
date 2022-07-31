85453ef7ec2a67e5411b5c3b553c099840954e1f
kirannbhatt <kiran.bhatt7638@gmail.com>
2022-08-01 00:21:55 +0545

    swell initialized

---
+++ src/Swell/Swell.ts
@@ -0,0 +1,13 @@
+const swell = require('swell-js');
+
+interface SwellConfig {
+    storeId: string;
+    publicKey: string;
+}
+
+export async function Swell({ storeId, publicKey }: SwellConfig) {
+    await swell.init(storeId, publicKey, {
+        useCamelCase: true
+    });
+    return swell
+}
